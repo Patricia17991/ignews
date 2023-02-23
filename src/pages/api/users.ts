1051c3914b5b4713eb5c6caba79f6440166d5232
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-02-23 18:29:50 -0300

    Update users.ts

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -7,7 +7,7 @@ export default (request: NextApiRequest, response: NextApiResponse) => {
         { id: 2, name: 'Patrícia'},
         { id: 3, name: 'Djanete'},
     ]
-
+//esta é uma rota puramente back-end, tudo aqui não é acessível ao cliente final
     return response.json(users)
-
+//todas as apis routes são executadas usando o conceito de serveless
 }
