e4a8afb895f9431679c14b6c5e509e2635b3ac94
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-02-23 19:53:10 -0300

    Update index.tsx

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,13 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn } from 'next-auth/client'
+import { signIn, useSession } from 'next-auth/client'
 
 
 export function SignInButton() {
-   const isUserLoggedIn = true;
+   const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
           type="button"
           className={styles.signInButton}
