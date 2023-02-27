1d06a4b8f9a8cb1e6cfb7abaff4faad40dfd328f
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2023-02-27 15:55:10 -0300

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,6 +31,12 @@ export default NextAuth({
                 q.Create(
                     q.Collection('users'),
                     { data: { email }}
+                ),
+                q.Get(
+                    q.Match(
+                       q.Index('user_by_email'),
+                         q.Casefold(user.email)
+                  )
                 )
                )
               )
