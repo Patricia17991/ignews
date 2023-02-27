69dc56eea16e0139b8b5018bc9d1d957c964b553
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2023-02-27 16:15:39 -0300

    Update index.tsx

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,26 @@
 import styles from './styles.module.scss'
-
+import { useSession, sigIn } from 'next-auth/client'
 interface SubscribeButtonProps {  
   priceId: string;
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    return (
+  const [session] = useSession()
+
+  function handleSubscribe() {
+     if(!session) {
+      sigIn('github')
+      return
+     }
+
+     
+  } 
+  
+  return (
         <button
           type="button"
           className={styles.subscribeButton}
+          onClick={handleSubscribe}
         >
           Subscribe now
         </button>
