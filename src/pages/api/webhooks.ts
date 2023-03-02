7eb831d050089cadd88ddcb04e05b04ec59b16c9
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2023-03-02 12:45:31 -0300

    Update webhooks.ts

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,18 @@
-import {NextApiRequest, NextApiResponse} from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream'
+
+//ler a requisição usando o Readable
+async function buffer(readable: Readable) {
+   const chunks = []
+
+   for await (const chunk of readable) {
+      chunks.push(
+         typeof chunk === "string" ? Buffer.from(chunk) : chunk 
+      )
+   }
+   return Buffer.concat(chunks)
+}
 
 export default  (req: NextApiRequest, res: NextApiResponse) => {
-   console.log('evento recebido')
-
    res.status(200).json({ ok: true})
 }
