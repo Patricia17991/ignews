6964c6d543722925b47afeeb4c38d7ce21f2f1fe
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2023-03-02 13:43:41 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import { SubscribeButton } from '../components/SubscribeButton'
-import styles from './home.module.scss'
-import { GetStaticProps} from 'next'
-import stripe from 'stripe'
+import { GetStaticProps } from 'next';
+import Head from 'next/Head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
@@ -15,40 +16,38 @@ export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-      <title>Home | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <main className={styles.contentContainer}> 
-         <section className={styles.hero}>
-            <span>👏Hey, welcome</span>
-            <h1>News about the <span>React </span>world.</h1>
-            <p>
-              Get access to all the publications <br />
-              <span>for {product.amount} month</span>
-            </p>
-            <SubscribeButton priceId={product.priceId} />
-         </section>
-         <img src="/images/avatar.svg" alt="Girl coding" />
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding." />
       </main>
     </>
   )
 }
-//tecla windows + . (pegar emogi no windows)
+
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1Mc7ZsKOCPprPNvHpQAOZq9L')
+  const price = await stripe.prices.retrieve('price_1IXrSxBy25ICnhnK1DPWCk0l')
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'USD'
     }).format(price.unit_amount / 100),
   }
-
   return {
     props: {
       product,
     },
-     revalidate: 60 * 60 * 24, //24 horas
-   }
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
-
